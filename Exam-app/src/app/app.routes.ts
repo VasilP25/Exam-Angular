b094a118beae47fcce31545f56d7bc34a68ffca9
VasilP25 <vasil.penev25@gmail.com>
2025-08-13 22:34:51 +0300

    Add register and login

--- Exam-app/src/app/app.routes.ts
+++ Exam-app/src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home-page.component/home-page.component';
+import { LoginComponent } from './login.component/login.component';
+import { RegisterComponent } from './register.component/register.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: Homepage },
+  { path: 'home', component: HomePageComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
 ];

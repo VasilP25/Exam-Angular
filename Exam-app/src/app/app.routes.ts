f370e9363b31ca32083b052f3525ee594afa4e55
VasilP25 <vasil.penev25@gmail.com>
2025-08-14 18:49:53 +0300

    Add create component

--- Exam-app/src/app/app.routes.ts
+++ Exam-app/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './home-page.component/home-page.component';
 import { LoginComponent } from './login.component/login.component';
 import { RegisterComponent } from './register.component/register.component';
+import { CreateComponent } from './create.component/create.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomePageComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'create', component: CreateComponent },
 ];
